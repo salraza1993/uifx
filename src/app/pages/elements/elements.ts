d222b24dfa8ce0f2b81d9ca3f07d5b3f9af3c74d
Salman Raza <salraza1993@gmail.com>
2026-01-19 20:17:22 +0400

    first commit

---
+++ src/app/pages/elements/elements.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements',
+  imports: [],
+  templateUrl: './elements.html',
+  styleUrl: './elements.css',
+})
+export class Elements {
+
+}
