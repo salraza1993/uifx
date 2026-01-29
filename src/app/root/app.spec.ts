02e4787648a7fd632b4e878d8aaaf8e5362b7dd9
Salman Raza <salraza1993@gmail.com>
2026-01-29 19:48:12 +0400

    angular version updated

--- src/app/root/app.spec.ts
+++ src/app/root/app.spec.ts
@@ -4,7 +4,7 @@ import { App } from '../root/app';
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App]
     }).compileComponents();
   });
 
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ifg-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UIFX!');
   });
 });
